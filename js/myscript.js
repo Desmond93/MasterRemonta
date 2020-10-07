4035f43b783ca457f75b2c91efaa60758b4badea
desmond93 <bondarenkomaxim93@gmail.com>
2020-10-07 23:30:03 +0300

    added masterRemonta

---
+++ js/myscript.js
@@ -0,0 +1,189 @@
+var windowWidth = window.innerWidth;
+var mql1 = window.matchMedia('all and (min-width: 577px)');
+var mql2 = window.matchMedia('all and (max-width: 576px)');
+var mql3 = window.matchMedia('all and (max-width: 400px)');
+
+$('.prices_top_1').on('click', function() {
+  if ($('.prices_top_1 .plus').text() == '+') { 
+    $('.table_1').css('display', 'table');
+    $('.table_1 tbody').css('display', 'table-row-group');    
+    $('.prices_top_1 .plus').html('-');
+    $('.prices_top_1').css('background-color', '#8fa79e');
+    $('.prices_top_1 .plus').css('color', '#1e453e'); 
+    $('.prices_top_1 h4').css('color', '#1e453e');       
+  }
+  else {
+    $('.table_1').css('display', 'none');
+    $('.table_1 tbody').css('display', 'none');
+    $('.prices_top_1 .plus').html('+');
+    $('.prices_top_1').css('background-color', '#1e453e');
+    $('.prices_top_1 .plus').css('color', '#d9e3dd'); 
+    $('.prices_top_1 h4').css('color', '#d9e3dd'); 
+}  
+});
+
+$('.prices_top_2').on('click', function() {
+  if ($('.prices_top_2 .plus').text() == '+') { 
+    $('.table_2').css('display', 'table');
+    $('.table_2 tbody').css('display', 'table-row-group');    
+    $('.prices_top_2 .plus').html('-');
+    $('.prices_top_2').css('background-color', '#8fa79e');
+    $('.prices_top_2 .plus').css('color', '#1e453e'); 
+    $('.prices_top_2 h4').css('color', '#1e453e');       
+  }
+  else {
+    $('.table_2').css('display', 'none');
+    $('.table_2 tbody').css('display', 'none');
+    $('.prices_top_2 .plus').html('+');
+    $('.prices_top_2').css('background-color', '#1e453e');
+    $('.prices_top_2 .plus').css('color', '#d9e3dd'); 
+    $('.prices_top_2 h4').css('color', '#d9e3dd'); 
+}  
+});
+
+$('.prices_top_3').on('click', function() {
+  if ($('.prices_top_3 .plus').text() == '+') { 
+    $('.table_3').css('display', 'table');
+    $('.table_3 tbody').css('display', 'table-row-group');    
+    $('.prices_top_3 .plus').html('-');
+    $('.prices_top_3').css('background-color', '#8fa79e');
+    $('.prices_top_3 .plus').css('color', '#1e453e'); 
+    $('.prices_top_3 h4').css('color', '#1e453e');       
+  }
+  else {
+    $('.table_3').css('display', 'none');
+    $('.table_3 tbody').css('display', 'none');
+    $('.prices_top_3 .plus').html('+');
+    $('.prices_top_3').css('background-color', '#1e453e');
+    $('.prices_top_3 .plus').css('color', '#d9e3dd'); 
+    $('.prices_top_3 h4').css('color', '#d9e3dd'); 
+}  
+});
+
+$('.prices_top_4').on('click', function() {
+  if ($('.prices_top_4 .plus').text() == '+') { 
+    $('.table_4').css('display', 'table');
+    $('.table_4 tbody').css('display', 'table-row-group');    
+    $('.prices_top_4 .plus').html('-');
+    $('.prices_top_4').css('background-color', '#8fa79e');
+    $('.prices_top_4 .plus').css('color', '#1e453e'); 
+    $('.prices_top_4 h4').css('color', '#1e453e');       
+  }
+  else {
+    $('.table_4').css('display', 'none');
+    $('.table_4 tbody').css('display', 'none');
+    $('.prices_top_4 .plus').html('+');
+    $('.prices_top_4').css('background-color', '#1e453e');
+    $('.prices_top_4 .plus').css('color', '#d9e3dd'); 
+    $('.prices_top_4 h4').css('color', '#d9e3dd'); 
+}  
+});
+
+$('.prices_top_5').on('click', function() {
+  if ($('.prices_top_5 .plus').text() == '+') { 
+    $('.table_5').css('display', 'table');
+    $('.table_5 tbody').css('display', 'table-row-group');    
+    $('.prices_top_5 .plus').html('-');
+    $('.prices_top_5').css('background-color', '#8fa79e');
+    $('.prices_top_5 .plus').css('color', '#1e453e'); 
+    $('.prices_top_5 h4').css('color', '#1e453e');       
+  }
+  else {
+    $('.table_5').css('display', 'none');
+    $('.table_5 tbody').css('display', 'none');
+    $('.prices_top_5 .plus').html('+');
+    $('.prices_top_5').css('background-color', '#1e453e');
+    $('.prices_top_5 .plus').css('color', '#d9e3dd'); 
+    $('.prices_top_5 h4').css('color', '#d9e3dd'); 
+}  
+});
+
+$('.prices_top_6').on('click', function() {
+  if ($('.prices_top_6 .plus').text() == '+') { 
+    $('.table_6').css('display', 'table');
+    $('.table_6 tbody').css('display', 'table-row-group');    
+    $('.prices_top_6 .plus').html('-');
+    $('.prices_top_6').css('background-color', '#8fa79e');
+    $('.prices_top_6 .plus').css('color', '#1e453e'); 
+    $('.prices_top_6 h4').css('color', '#1e453e');       
+  }
+  else {
+    $('.table_6').css('display', 'none');
+    $('.table_6 tbody').css('display', 'none');
+    $('.prices_top_6 .plus').html('+');
+    $('.prices_top_6').css('background-color', '#1e453e');
+    $('.prices_top_6 .plus').css('color', '#d9e3dd'); 
+    $('.prices_top_6 h4').css('color', '#d9e3dd'); 
+}  
+});
+
+$('.prices_top_7').on('click', function() {
+  if ($('.prices_top_7 .plus').text() == '+') { 
+    $('.table_7').css('display', 'table');
+    $('.table_7 tbody').css('display', 'table-row-group');    
+    $('.prices_top_7 .plus').html('-');
+    $('.prices_top_7').css('background-color', '#8fa79e');
+    $('.prices_top_7 .plus').css('color', '#1e453e'); 
+    $('.prices_top_7 h4').css('color', '#1e453e');       
+  }
+  else {
+    $('.table_7').css('display', 'none');
+    $('.table_7 tbody').css('display', 'none');
+    $('.prices_top_7 .plus').html('+');
+    $('.prices_top_7').css('background-color', '#1e453e');
+    $('.prices_top_7 .plus').css('color', '#d9e3dd'); 
+    $('.prices_top_7 h4').css('color', '#d9e3dd'); 
+}  
+});
+
+$('.prices_top_8').on('click', function() {
+  if ($('.prices_top_8 .plus').text() == '+') { 
+    $('.table_8').css('display', 'table');
+    $('.table_8 tbody').css('display', 'table-row-group');    
+    $('.prices_top_8 .plus').html('-');
+    $('.prices_top_8').css('background-color', '#8fa79e');
+    $('.prices_top_8 .plus').css('color', '#1e453e'); 
+    $('.prices_top_8 h4').css('color', '#1e453e');       
+  }
+  else {
+    $('.table_8').css('display', 'none');
+    $('.table_8 tbody').css('display', 'none');
+    $('.prices_top_8 .plus').html('+');
+    $('.prices_top_8').css('background-color', '#1e453e');
+    $('.prices_top_8 .plus').css('color', '#d9e3dd'); 
+    $('.prices_top_8 h4').css('color', '#d9e3dd'); 
+}  
+});
+
+function screen_change() {
+if (mql2.matches) {
+    $('.main_page .first .rhombus_text .left p').html('Современный ремонт -<br>это лаконичность,<br>функциональность, комфорт.<br>Это умение создавать<br>прекрасное там,<br>где его пока нет.');
+    $('.main_page .first .rhombus_text .left').css('padding-right', '5px');
+    $('.main_page .first .rhombus_text .right p').html('Современный дизайн -<br>это вдохновение, стиль,<br>эстетика,игра цветов.<br>Это искусство видеть<br>прекрасное там,<br>где его пока нет.');
+    $('.main_page .first .rhombus_text .right').css('padding-left', '5px');
+    $('.main_page .first .rhombus_text h3').css('padding-top', '0');
+}
+if (mql1.matches) {
+    $('.main_page .first .rhombus_text .left p').html('Современный ремонт выходит<br>за рамки привычного бытового<br>понимания. Ремонт и отделка<br>помещений сегодня - это создание<br>нового дизайна, не просто<br>красивого внешне, но и<br>функционального по<br>назначению, отвечающего<br>требованиям<br>современной<br>жизни');
+    $('.main_page .first .rhombus_text .left').css('padding-right', '15px');
+    $('.main_page .first .rhombus_text .right p').html('Современный дизайн требует<br>продуманного стиля оформления,<br>красоты интерьера, подчёркивающих<br>вкусы владельца. Качественная<br>отделка решает вопрос<br>соответствия в интерьере.<br>Любой объём<br>строительных и<br> отделочных работ<br>требует знаний<br>технологий<br>процесса');
+    $('.main_page .first .rhombus_text .right').css('padding-left', '15px');
+    $('.main_page .first .rhombus_text .line').css('display', 'block');
+    $('.main_page .first .rhombus_text h3').css('padding-top', '0');
+}
+if (mql3.matches) {
+    $('.main_page .first .rhombus_text .left p').html('');
+    $('.main_page .first .rhombus_text .left').css('padding-right', '15px');    
+    $('.main_page .first .rhombus_text .right p').html('');
+    $('.main_page .first .rhombus_text .right').css('padding-left', '15px');
+    $('.main_page .first .rhombus_text .line').css('display', 'none');
+    $('.main_page .first .rhombus_text h3').css('padding-top', '70px');
+}
+}
+
+screen_change();
+
+$(window).resize(function() {
+var windowWidth = window.innerWidth;
+screen_change();
+});
